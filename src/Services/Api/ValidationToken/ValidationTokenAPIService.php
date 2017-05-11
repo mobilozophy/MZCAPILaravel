2e10b9ae3412e4af6374937a9701e00076f447ae
Jeffrey Wray <jwray@mobilozophy.com>
2017-05-11 14:39:17 -0400

    validation api

---
+++ src/Services/Api/ValidationToken/ValidationTokenAPIService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Mobilozophy\MZCAPILaravel\Services\Api\ValidationToken;
+
+use Mobilozophy\MZCAPILaravel\Services\Api\AbstractAPIService;
+use Mobilozophy\MZCAPILaravel\Services\Api\Credentials;
+
+class ValidationTokenAPIService extends AbstractAPIService
+{
+    const ENDPOINT = 'validation-token';
+
+
+    /**
+     * Send a request to retrieve a store.
+     *
+     * @param Credentials $credentials
+     * @param integer $accountId
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function get(Credentials $credentials, $resource, $resource_id)
+    {
+        $requestUrl = $this->getEndpointRequestUrl([$resource, $resource_id]);
+        return $this->httpClient->get($requestUrl, [
+            'headers' => $credentials->getHeaders(),
+            'auth' => $credentials->toArray()
+        ]);
+    }
+
+
+
+    /**
+     * Get base API request URL with additional segments.
+     *
+     * @param mixed $segments
+     *
+     * @return string
+     */
+    protected function getBaseRequestUrl($segments = null)
+    {
+        if (is_array($segments)) {
+            $segments = implode('/', $segments);
+        }
+
+        $baseUrl = env('MZCAPI_BASEURL');
+
+        return $baseUrl . '/' . $segments;
+    }
+}
---
+++ src/Services/ValidationToken/ValidationTokenService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Mobilozophy\MZCAPILaravel\Services\ValidationToken;
+
+use Mobilozophy\MZCAPILaravel\Services\Api\ValidationToken\ValidationTokenAPIService;
+use Mobilozophy\MZCAPILaravel\Services\ServiceBase;
+
+class ValidationTokenService extends ServiceBase
+{
+
+    public function __construct(
+        ValidationTokenAPIService $validationTokenAPIService
+    ) {
+        $this->apiService = $validationTokenAPIService;
+    }
+
+    /**
+     * @param      $resource
+     * @param      $resource_id
+     * @param null $account_uuid
+     * @param bool $scope
+     *
+     * @return bool
+     */
+    public function getToken($resource, $resource_id, $account_uuid = null, $scope = false)
+    {
+        try {
+            $response = $this->apiService->get(
+                $this->getSubAccountCredentials($account_uuid,$scope), $resource,$resource_id
+            );
+            if ($response->getStatusCode() == 200) {
+                return json_decode($response->getBody()->getContents());
+            } else {
+                return false;
+            }
+        } catch (\Exception $e)
+        {
+            return false;
+        }
+    }
+
+
+
+}
