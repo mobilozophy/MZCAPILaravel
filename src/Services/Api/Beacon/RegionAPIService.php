7b268d3f9a6562022c0f6e6a176be789c5b1c421
Jeffrey Wray <jwray@mobilozophy.com>
2018-01-10 16:35:17 -0500

    Adding region service.

---
+++ src/Services/Api/Beacon/RegionAPIService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mobilozophy\MZCAPILaravel\Services\Api\Redemption;
+
+use Mobilozophy\MZCAPILaravel\Services\Api\Credentials;
+use Mobilozophy\MZCAPILaravel\Services\Api\MZCAPIAPIService;
+
+class RegionAPIService extends MZCAPIAPIService
+{
+    const ENDPOINT = 'beacons/regions';
+}
---
+++ src/Services/Beacon/RegionService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mobilozophy\MZCAPILaravel\Services\Beacon;
+
+use Mobilozophy\MZCAPILaravel\Services\Api\Redemption\RegionAPIService;
+use Mobilozophy\MZCAPILaravel\Services\ServiceBase;
+
+/**
+ * Class RegionService
+ * @package Mobilozophy\MZCAPILaravel\Services\Beacon
+ */
+class RegionService extends ServiceBase
+{
+
+    /**
+     * RegionService constructor.
+     *
+     * @param RegionAPIService $regionAPIService
+     */
+    public function __construct(RegionAPIService $regionAPIService) {
+        $this->apiService = $regionAPIService;
+    }
+
+}
