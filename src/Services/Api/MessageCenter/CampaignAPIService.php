fdf07beb1dc2667a282f55464b3dd140f8e4d28c
Jeff Wray <jwray@mobilozophy.com>
2019-06-12 15:51:56 -0400

    Adding missing API Service class

---
+++ src/Services/Api/MessageCenter/CampaignAPIService.php
@@ -0,0 +1,8 @@
+<?php
+namespace Mobilozophy\MZCAPILaravel\Services\Api\MessageCenter;
+use Mobilozophy\MZCAPILaravel\Services\Api\MZCAPIAPIService;
+
+class CampaignAPIService extends MZCAPIAPIService
+{
+    const ENDPOINT = 'message-center/campaigns';
+}
