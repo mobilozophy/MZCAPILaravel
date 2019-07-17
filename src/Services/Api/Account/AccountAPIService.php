1ede6d4306cab2e3edd0ea8ec6cf937dd8db0f7e
Hrvoje Gazibara <hgazibara@gmail.com>
2019-07-18 00:04:47 +0200

    Extend the account services to support application retrieval.

--- src/Services/Api/Account/AccountAPIService.php
+++ src/Services/Api/Account/AccountAPIService.php
@@ -8,4 +8,20 @@ use Mobilozophy\MZCAPILaravel\Services\Api\MZCAPIAPIService;
 class AccountAPIService extends MZCAPIAPIService
 {
     const ENDPOINT = 'accounts';
+
+    public function getApplication(Credentials $credentials, $accountUuid)
+    {
+        $requestUrl = $this->composeGetApplicationRequestUrl($accountUuid);
+
+        return $this->httpClient->get($requestUrl, [
+            'headers' => $credentials->getHeaders(),
+            'auth' => $credentials->toArray()
+        ]);
+    }
+
+    public function composeGetApplicationRequestUrl($accountUuid)
+    {
+        $requestUrl = $this->getEndpointRequestUrl([$accountUuid, 'app']);
+        return $requestUrl;
+    }
 }
