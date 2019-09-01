d3a004eb6d4b296a8806158918e25171d38f87d9
Hgazibara <hgazibara@gmail.com>
2019-09-01 13:40:50 +0200

    Extend the ApplicationAPIService class with a method for uploading an app cover image.

--- src/Services/Api/Application/ApplicationAPIService.php
+++ src/Services/Api/Application/ApplicationAPIService.php
@@ -26,4 +26,26 @@ class ApplicationAPIService extends MZCAPIAPIService
         $requestUrl = $this->getEndpointRequestUrl([$appId, 'featured', 'loyalty', $loyaltyId]);
         return $this->httpClient->post($requestUrl, $this->generateOptions($credentials));
     }
+
+    public function setCoverImage(Credentials $credentials, $appId, $file)
+    {
+        $requestUrl = $this->getEndpointRequestUrl([$appId, 'hero-image']);
+
+        $image_path = $file->getPathname();
+        $image_mime = $file->getmimeType();
+        $image_org = $file->getClientOriginalName();
+
+        return $this->httpClient->post($requestUrl, [
+            'headers' => $credentials->getHeaders(),
+            'auth' => $credentials->toArray(),
+            'multipart' => [
+                [
+                    'name' => 'file',
+                    'filename' => $image_org,
+                    'Mime-Type' => $image_mime,
+                    'contents' => fopen($image_path, 'r')
+                ]
+            ]
+        ]);
+    }
 }
